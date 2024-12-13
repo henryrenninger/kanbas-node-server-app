68d5243db06cc83e54793c617f3044e014713715
henryrenninger <henryrenninger@gmail.com>
2024-12-12 22:44:41 -0500

    assignment 5 stuff

--- Kanbas/Assignments/routes.js
+++ Kanbas/Assignments/routes.js
@@ -1,14 +1,16 @@
-import * as dao from "./dao.js";
+import * as assignmentsDao from "./dao.js";
 export default function AssignmentsRoutes(app) {
     app.delete("/api/assignments/:assignmentId", async (req, res) => {
         const { assignmentId } = req.params;
-        const status = await dao.deleteAssignment(assignmentId);
+        const status = await assignmentsDao.deleteAssignment(assignmentId);
         res.send(status);
     });
+
     app.put("/api/assignments/:assignmentId", async (req, res) => {
         const { assignmentId } = req.params;
         const assignmentUpdates = req.body;
-        const status = await dao.updateAssignment(assignmentId, assignmentUpdates);
+        const status = await assignmentsDao.updateAssignment(assignmentId, assignmentUpdates);
         res.send(status);
     });
+
 }
--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -1,16 +1,20 @@
-import database from "../Database/index.js";
+import Database from '../Database/index.js';
 export function enrollUserInCourse(userId, courseId) {
-    const { enrollments } = database;
-    enrollments.push({
-        _id: Date.now().toString(), user: userId, course: courseId,
-    });
+  const { enrollments } = Database;
+  enrollments.push({
+    _id: Date.now().toString(),
+    user: userId,
+    course: courseId,
+  });
 }
+
 export function unenrollUserInCourse(userId, courseId) {
-    const { enrollments } = database;
-    database.enrollments = enrollments.filter(
-        (enrollment) => !(enrollment.user === userId && enrollment.course === courseId)
-    );
+  const { enrollments } = Database;
+  Database.enrollments = enrollments.filter(
+    (enrollment) => !(enrollment.user === userId && enrollment.course === courseId)
+  );
 }
+
 export function fetchAllEnrollments() {
-    return database.enrollments;
+  return Database.enrollments;
 }
